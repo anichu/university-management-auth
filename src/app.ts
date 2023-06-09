53aeb1b65958cb676ee11b506b64acdd7177e225
anichu <anis.cse5.bu@gmail.com>
2023-06-09 23:59:39 +0600

    create user service and controllers

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
-
+import userService from './app/modules/user/user.service'
+import usersRoutes from './app/modules/user/user.route'
 const app: Application = express()
 
 app.use(cors())
@@ -12,8 +13,19 @@ app.use(
     extended: true,
   })
 )
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!')
+// Application routes
+
+app.use('/api/v1/users', usersRoutes)
+
+app.get('/', async (req: Request, res: Response) => {
+  res.send('Hello')
+})
+
+app.post('/', async (req: Request, res: Response) => {
+  const data = req.body
+  console.log(data)
+  const user = await userService.createUser(data)
+  res.send(user)
 })
 
 export default app
---
+++ src/app/modules/user/user.utils.ts
@@ -0,0 +1,26 @@
+import User from './user.model'
+
+export const findLastUserId = async () => {
+  const lastUser = await User.findOne(
+    {},
+    {
+      id: 1,
+      _id: 0,
+    }
+  )
+    .sort({
+      createdAt: -1,
+    })
+    .lean()
+
+  return lastUser?.id
+}
+
+export const generateUserId = async () => {
+  const currentId = (await findLastUserId()) || (0).toString().padStart(5, '0')
+  let incrementId = (parseInt(currentId) + 1).toString()
+  const incrementIdLength = incrementId.length
+  incrementId = incrementId.padStart(6 - incrementIdLength, '0')
+  console.log(incrementId)
+  return incrementId
+}
